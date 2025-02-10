b27a7865d6661aac98dab7ea7603369ae28a3c29
Ваше Имя <.email@example.com>
2025-02-10 23:07:02 +0300

    fix

--- src/components/pages/Shop/Shop.tsx
+++ src/components/pages/Shop/Shop.tsx
@@ -1,13 +1,15 @@
 // import Button from '@sergeri-jr25/shared1234/dist/components/Button'
-import { Button, CardProduct } from '@sergeri-jr25/shared1234'
+// import { Button, CardProduct } from '@sergeri-jr25/shared1234'
+// import { FC } from 'react'
+
 import { FC } from 'react'
 
 const Shop: FC = () => {
 	return (
 		<div>
 			<h1>Shop</h1>
-			<Button label='' onClick={() => console.log('')}></Button>
-			<CardProduct userName='asd' />
+			{/* <Button label='' onClick={() => console.log('')}></Button>
+			<CardProduct userName='asd' /> */}
 		</div>
 	)
 }
